93bfe1724f62caeba7e67f6bec2830d46db1cade
Luis Marin <luismarin@netlabs.com.ar>
2018-08-21 16:56:31 -0300

    Ajustar controles sobre Listar y Agregar Barco

--- src/resources/lang/es/boats.php
+++ src/resources/lang/es/boats.php
@@ -12,7 +12,7 @@ return [
 	'boats_date_arrive' => 'Fecha de arribo',
 	'boats_new_arrive'=> 'Nuevo Arribo',
 	'boats_load_lote'=> 'Cargar lote',
-    'matricula' => 'Matricula',
+    'matricula' => 'Matrícula',
     'status' => 'Estado',
     'actions' => [
         'title' => 'Acciones',
@@ -26,13 +26,13 @@ return [
     'seller' => [
         'cuit' => 'CUIT'
     ],
-    'pending_message' => 'El barco esta pendiente de aprobación',
+    'pending_message' => 'El barco está pendiente de aprobación',
     'accept_boat' => 'Aprobar Barco',
     'reject_boat' => 'Rechazar Barco',
-    'accept_boat_msg' => 'El Barco fue aprobado',
-    'accept_boat_confirm_msg' => 'Esta seguro que desea aprobar al Barco?',
-    'reject_boat_msg' => 'El Barco fue rechazado',
-    'reject_boat_confirm_msg' => 'Esta seguro que desea rechazar al Barco?',
+    'accept_boat_msg' => 'El Barco fué aprobado',
+    'accept_boat_confirm_msg' => '¿Está seguro que desea aprobar al Barco?',
+    'reject_boat_msg' => 'El Barco fué rechazado',
+    'reject_boat_confirm_msg' => '¿Está seguro que desea rechazar al Barco?',
     'no_boats' => 'No hay barcos aprobados.',
     'edit_arrive' => 'Editar Arribo',
 
