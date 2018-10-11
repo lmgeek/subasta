b00ccb9646c0d66471d6c35b4125ccbdaea08c8d
Luis Marin <luismarin@netlabs.com.ar>
2018-10-11 13:08:30 -0300

    delete deploy sql old

--- deploy/sql/V5__change_doble_float_columns.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `auctions`
-MODIFY COLUMN `start_price`  float(8,2) NOT NULL AFTER `start`,
-MODIFY COLUMN `end_price`  float(8,2) NOT NULL AFTER `end`;
-
-ALTER TABLE `bids`
-MODIFY COLUMN `price`  float(8,2) NOT NULL AFTER `amount`;
-
-ALTER TABLE `private_sales`
-MODIFY COLUMN `price`  float(8,2) NOT NULL AFTER `amount`;
--- deploy/sql/V6__add_field__weigths_product_table.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `products`
-CHANGE COLUMN `weigth` `weigth_small` float(10,2) NOT NULL AFTER `unit`,
-ADD COLUMN `weigth_medium` float(10,2) NOT NULL AFTER `weigth_small`,
-ADD COLUMN `weigth_big` float(10,2) NULL AFTER `weigth_medium`;
