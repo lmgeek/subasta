263cf4f4299e11583dde336988785471ecc4f4c0
Diego Weinstein <diegow@netlabs.com.ar>
2019-01-12 16:38:48 -0300

    arregle V11__table_port.sql

--- deploy/sql/V11__table_port.sql
+++ deploy/sql/V11__table_port.sql
@@ -2,9 +2,9 @@ CREATE TABLE `port` (
 `id`  int(100) NOT NULL AUTO_INCREMENT ,
 `name`  varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 PRIMARY KEY (`id`)
-)
+);
 
-INSERT INTO port (`name`,) VALUES ('Mar del Plata');
-INSERT INTO port (`name`,) VALUES ('Buenos Aires');
-INSERT INTO port (`name`,) VALUES ('Puerto Madryn');
-INSERT INTO port (`name`,) VALUES ('General Lavalle');
+INSERT INTO port (`name`) VALUES ('Mar del Plata');
+INSERT INTO port (`name`) VALUES ('Buenos Aires');
+INSERT INTO port (`name`) VALUES ('Puerto Madryn');
+INSERT INTO port (`name`) VALUES ('General Lavalle');
