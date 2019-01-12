6d37587349ff2512c91ba2215e18b9dff0db8619
Diego Weinstein <diegow@netlabs.com.ar>
2019-01-12 16:21:55 -0300

    arregle los indices de deploy/sql y sume stacks

---
+++ stacks/deploy_dev.sh
@@ -0,0 +1,10 @@
+
+if [ "${MAIL_TO}" == "" ]
+then
+  echo "Falta dar de alta el mail destino"
+  echo ""
+  echo "export MAIL_TO=mimail@netlabs.com.ar"
+  exit 255
+fi
+
+docker stack deploy -c dev.yml internos --with-registry-auth
