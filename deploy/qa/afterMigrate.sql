0132ea06351f743c50554171a41004ea99826d2d
Diego Weinstein <diegow@netlabs.com.ar>
2019-01-31 11:19:15 -0300

    cambie en afterMigrate el nick de diegow@netlabs.com.ar, le puse diegow

--- deploy/qa/afterMigrate.sql
+++ deploy/qa/afterMigrate.sql
@@ -15,7 +15,7 @@ BEGIN
   IF NOT EXISTS ( SELECT * FROM users
     WHERE email = 'diegow@netlabs.com.ar' ) THEN
       INSERT INTO users (`name`,`lastname`,`email`,`password`,`type`,`status`,`hash`,`active_mail`,`nickname`)
-      VALUES ('Diego','Weinstein','diegow@netlabs.com.ar','$2y$10$chZ5pITb5Q7e/HQIo6qjeudB.QQ8.Zby.vpOgSjdqjNDbVasz4Rze','internal','approved','NO_aplica',1,'admin');
+      VALUES ('Diego','Weinstein','diegow@netlabs.com.ar','$2y$10$chZ5pITb5Q7e/HQIo6qjeudB.QQ8.Zby.vpOgSjdqjNDbVasz4Rze','internal','approved','NO_aplica',1,'diegow');
   END IF;
 
 END $$
