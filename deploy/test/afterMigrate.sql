f24272a12017b014e57adea94ff781f18b4dccd2
Diego Weinstein <diegow@netlabs.com.ar>
2019-02-07 14:14:06 -0300

    agregue el afterMigrate de test

---
+++ deploy/test/afterMigrate.sql
@@ -0,0 +1,6 @@
+
+
+
+# Dejar esto al final ya que lo uso para verificar que termino el deploy
+INSERT INTO users (id,`name`,`lastname`,`email`,`password`,`type`,`status`,`hash`,`active_mail`,`nickname`)
+VALUES (999,'findeploy','deploy','diegow@netlabs.com.ar','$2y$10$chZ5pITb5Q7e/HQIo6qjeudB.QQ8.Zby.vpOgSjdqjNDbVasz4Rze','internal','approved','NO_aplica',1,'findeploy');
