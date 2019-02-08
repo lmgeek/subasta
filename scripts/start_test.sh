15af8997e3f660e91177d379f8da2c35acb718f1
Diego Weinstein <diegow@netlabs.com.ar>
2019-02-08 11:17:05 -0300

    subi el start_test.sh

---
+++ scripts/start_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apache2
+
+until curl -s --connect-timeout 1 -o /dev/null http://localhost
+do
+	echo "curl localhost: sleeping 1"
+	sleep 1
+done
+
+DEST_DIR=/var/www/html/public/results
+
+if [ -d ${DEST_DIR} ]
+then
+  rm -rf ${DEST_DIR}
+fi
+
+php /wait_for.php findeploy 999
+
+mkdir ${DEST_DIR}
+
+/var/www/html/vendor/bin/phpunit /var/www/html/tests \
+  --coverage-clover ${DEST_DIR}/coverage-clover.xml \
+  --coverage-html ${DEST_DIR}/coverage-htm \
+  --log-junit ${DEST_DIR}/log-junit.xml \
+  --testdox-html ${DEST_DIR}/agile.html
+
+touch ${DEST_DIR}/fin
