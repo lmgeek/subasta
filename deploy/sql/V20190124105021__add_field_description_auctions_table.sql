82a06c44b5f6ab5f355322c247237daaca516470
Rafael Alvarez <rafaela@netlabs.com.ar>
2019-02-15 09:08:17 -0300

    midificacion del sql de descripcion

--- deploy/sql/V20190124105021__add_field_description_auctions_table.sql
+++ deploy/sql/V20190124105021__add_field_description_auctions_table.sql
@@ -1,22 +1,23 @@
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS upgrade_database_20190124105021 $$
+DROP PROCEDURE IF EXISTS upgrade_database_20190110130000 $$
 
-CREATE PROCEDURE upgrade_database_20190124105021()
+CREATE PROCEDURE upgrade_database_20190110130000()
 
 BEGIN
 
-      IF NOT EXISTS ( SELECT * FROM information_schema.`COLUMNS`
-        WHERE
-          TABLE_SCHEMA='subastas'
-          AND table_name='auctions'
-          AND column_name='description'
-        ) THEN
-            ALTER TABLE `auctions` ADD COLUMN `description` varchar(1000) NOT NULL;
-      END IF;
-      END $$
-CALL upgrade_database_20190124105021() $$
+IF NOT EXISTS ( SELECT * FROM information_schema.`COLUMNS`
+WHERE
+  TABLE_SCHEMA='subastas'
+  AND table_name='auctions'
+  AND column_name='description') THEN
+  ALTER TABLE auctions ADD COLUMN description varchar(1000) NOT NULL DEFAULT '';
+END IF;
 
-DROP PROCEDURE upgrade_database_20190124105021() $$
+END $$
+
+CALL upgrade_database_20190110130000() $$
+
+DROP PROCEDURE upgrade_database_20190110130000 $$
 
 DELIMITER ;
