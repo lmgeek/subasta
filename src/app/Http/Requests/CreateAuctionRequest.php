81bbf69cf0026514913587d8c5fb2be640b8a364
Luis Marin <tsuluismarin@gmail.com>
2019-02-21 11:01:09 -0300

    Diseñar la relación compra - oferta

--- src/app/Http/Requests/CreateAuctionRequest.php
+++ src/app/Http/Requests/CreateAuctionRequest.php
@@ -38,7 +38,7 @@ class CreateAuctionRequest extends Request
             'fechaInicio'  => 'required|date_format:d/m/Y H:i|after:'.date('d/m/Y H:i'),
             'fechaTentativa'  => 'required|date_format:d/m/Y H:i|after:'.date('d/m/Y H:i'),
             'fechaFin'    => 'required|date_format:d/m/Y H:i|after:fechaInicio',
-            'startPrice' => 'required|regex:/^\d{1,}(\,\d+)?$/|min:1||auction_price_greater_than:endPrice',
+            'startPrice' => 'required|regex:/^\d{1,}(\,\d+)?$/|min:1|auction_price_greater_than:endPrice',
             'endPrice'   => 'required|regex:/^\d{1,}(\,\d+)?$/|min:1',
             'amount'     => 'required|numeric|min:1',
             'descri'   => 'required|min:120|max:1000',
