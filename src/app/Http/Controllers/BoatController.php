75fe1e4d0bc03534ef1d6b54ecfab4217d6f23af
german <germanb@netlabs.com.ar>
2019-03-14 17:24:23 -0300

    suprimir url sellerboat y sellerboat/create

--- src/app/Http/Controllers/BoatController.php
+++ src/app/Http/Controllers/BoatController.php
@@ -63,16 +63,6 @@ class BoatController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
