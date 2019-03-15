ed511ef90af513b134cc9ff6aa1943baa06bc894
german <germanb@netlabs.com.ar>
2019-03-15 14:30:47 -0300

    modificacion de pruebas unitarias de barco

--- src/tests/03CreateNewBoatTest.php
+++ src/tests/03CreateNewBoatTest.php
@@ -20,19 +20,23 @@ class CreateNewBoatTest extends TestCase
     function successfullRegisterBoat()
     {
         $id = $this->getTheLastIdInsertedInTheSeller();
-        $this->unit($id,'visit','/sellerboat/create');
-        $this->unit($id,'see','','Agregar Barco');
+        $this->unit($id,'visit','/barcos');
+        $this->unit($id,'click','','','Nuevo Barco');
+        $this->unit($id,'see','','Nombre del barco');
         $this->unit($id,'type','','Venezuela Libre','name');
         $this->unit($id,'type','','V2019','matricula');
         $this->unit($id,'select','','2','port');
-        $this->unit($id,'press','','Guardar');
+        $this->unit($id,'press','','','Guardar');
+
     }
 
     /** @test */
     function cancelBottonWhenCreatingBoat()
     {
         $id = $this->getTheLastIdInsertedInTheSeller();
-        $this->unit($id,'visit','/sellerboat/create');
+        $this->unit($id,'visit','/barcos');
+        $this->unit($id,'click','','','Nuevo Barco');
+        $this->unit($id,'see','','Cancelar');
         $this->unit($id,'click','','','Cancelar');
         $this->unit($id,'see','','Mis Barcos');
         $this->unit($id,'responseOK');
